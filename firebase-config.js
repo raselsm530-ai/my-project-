31f1adce3cbd8ff0c141f56eaa9b8945ee4da52c
raselsm530-ai <raselsm530@gmail.com>
2025-12-22 08:24:13 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCH_qTmU7QIUC4RKoYEX7QaXpPUNSN8mBI",
-  authDomain: "smart-earning-platform-bd.firebaseapp.com",
-  databaseURL: "https://smart-earning-platform-bd-default-rtdb.firebaseio.com",
-  projectId: "smart-earning-platform-bd",
-  storageBucket: "smart-earning-platform-bd.firebasestorage.app",
-  messagingSenderId: "1076796437288",
-  appId: "1:1076796437288:web:b6a6497cf4795dc3c0561d",
-  measurementId: "G-ZQ5BT4LT2D"
+   // your config here
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
